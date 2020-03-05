3ad932215fae4606b4665707669ad34d045dc206
abishekjayaram <61828046+abishekjayaram@users.noreply.github.com>
2020-03-05 16:16:57 +0530

    simple c file

---
+++ arithmetic3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+float c,d,e,f,m;
+int a,b;
+printf("enter the value of a and b\n");
+scanf("%d%d",&a,&b);
+c=a+b;
+d=a+b;
+e=a*b;
+f=a/b;
+m=a%b;
+printf("c=%f",c);
+printf("d=%f",d);
+printf("e=%f",e);
+printf("f=%f",f);
+printf("m=%f",m);
+}
+
+
